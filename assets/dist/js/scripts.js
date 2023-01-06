5c6137b1c2525abb15ffabee609496d57adeeace
Tshegofatso Motswagae <tshegofatsomotswagae@hotmail.com>
2023-01-06 22:20:18 +0200

    Updated feeds structure

--- assets/dist/js/scripts.js
+++ assets/dist/js/scripts.js
@@ -6,9 +6,29 @@ class RssFeedModel {
     }
 }
 
-function trycatch(func, fail) {
-    try { return func() }
-    catch(e) { return fail }
+function tryCatch(func, fail) {
+    try { 
+        return func();
+    }
+    catch(e) { 
+        return fail;
+    }
+}
+
+function getDropDownLinks(feed) {
+    let links = "";
+    try {
+        $.each(feed.categories, function (i, feedCategory) {
+            links += '<li><a class=\"dropdown-item\" href=\"?id='+ feedCategory.id +'\">'+ feedCategory.title +'</a></li>';
+        });
+        return links;
+    }
+    catch(e) { 
+        console.log(e);
+    }
+    finally {
+        links = "";
+    }
 }
 
 let rssFeeds = [];
@@ -21,15 +41,34 @@ $.ajax({
     dataType: 'json',
     success: function(json){
         $.each(json, function (i, item) {
-            rssFeeds.push(new RssFeedModel(item.id, item.title, item.url));
+            if (item.id == 3 || item.id == 6) {
+                $.each(item.categories, function (ii, category) {
+                    rssFeeds.push(new RssFeedModel(category.id, category.title, category.url));
+                });
+            }
+            else {
+                rssFeeds.push(new RssFeedModel(item.id, item.title, item.url));
+            }
+            
             $('#feeds').append(
-                '<li class="nav-item">' + 
-                    '<a class="nav-link" href=\"?id='+ item.id +'\">'+ item.title +'</a>' +
-                '</li>'
+                (item.id == 3 || item.id == 6 ?
+                    '<li class=\"nav-item dropdown\">' +
+                        '<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">' +
+                            item.title +
+                        '</a>' +
+                        '<ul class="dropdown-menu">' +
+                            getDropDownLinks(item) +
+                        '</ul>' +
+                    '</li>'
+                :
+                    '<li class="nav-item">' + 
+                        '<a class="nav-link" href=\"?id='+ item.id +'\">'+ item.title +'</a>' +
+                    '</li>'
+                )
             );
         });
 
-        let rssFeed = rssFeeds.find(element => element.id === (id === '' ? 0 : Number(id)));
+        let rssFeed = rssFeeds.find(element => element.id == (id == '' ? 0 : Number(id)));
 
         if (rssFeed != null) {
             $.ajax({
@@ -45,18 +84,18 @@ $.ajax({
                                 title: $this.find("title").text(),
                                 link: $this.find("link").text(),
                                 description: $this.find("description").text(),
-                                contentEncoded: trycatch(() => $this.find('content\\:encoded, encoded').text(), null),
+                                contentEncoded: tryCatch(() => $this.find('content\\:encoded, encoded').text(), null),
                                 pubDate: $this.find("pubDate").text(),
-                                imgUrl0: trycatch(() => $this.find('image').text(), null),
-                                imgUrl1: trycatch(() => $this.find('media\\:content, content').attr('url'), null),
-                                imgUrl2: trycatch(() => $this.find('enclosure, enclosure').attr('url'), null),
-                                imgUrl3: trycatch(() => $this.find('media\\:thumbnail, thumbnail').attr('url'), null)
+                                imgUrl0: tryCatch(() => $this.find('image').text(), null),
+                                imgUrl1: tryCatch(() => $this.find('media\\:content, content').attr('url'), null),
+                                imgUrl2: tryCatch(() => $this.find('enclosure, enclosure').attr('url'), null),
+                                imgUrl3: tryCatch(() => $this.find('media\\:thumbnail, thumbnail').attr('url'), null)
                             };
 
                         $('#cards').append(
                             '<div class="col">' +
                                 '<div class=\"card shadow-sm\">' +
-                                    (rssFeed.id == 0 || rssFeed.id == 5 || rssFeed.id == 7 || rssFeed.id == 8 || rssFeed.id == 9 || rssFeed.id == 10 ?
+                                    (rssFeed.id == 0 || rssFeed.id == 4 || rssFeed.id == 6.1 || rssFeed.id == 6.2 || rssFeed.id == 6.3 || rssFeed.id == 6.4 ?
                                         ""
                                     :
                                         (item.imgUrl0 == "" || item.imgUrl0 == null || item.imgUrl0 == undefined ?
@@ -85,7 +124,7 @@ $.ajax({
                                     ) +
                                     '<div class=\"card-body\">' +
                                         '<h5 class=\"card-title\">'+ item.title +'</h5>' +
-                                        '<p class=\"card-text\">'+ (rssFeed.id != 5 ? item.description : item.contentEncoded) +'</p>' +
+                                        '<p class=\"card-text\">'+ (rssFeed.id == 4 ? item.contentEncoded : item.description) +'</p>' +
                                         '<div class=\"d-flex justify-content-between align-items-center\">' +
                                             '<div class="btn-group\">' +
                                                 '<a href=\"'+ item.link +'\" class=\"btn btn-sm btn-outline-secondary\" target=\"_blank\">View</a>' +
